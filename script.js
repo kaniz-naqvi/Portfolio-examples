bc6e16d9c4af05e0900148b1e8aa709694f3a449
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2024-12-20 12:37:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let container=document.querySelector(".portfolio-container");
+container.innerHTML=`
+ <div class="portfolio-item">
+            <h2>Mechanical Engineer Portfolio</h2>
+            <p>Click below to view the Mechanical Engineer Portfolio</p>
+            <a href="portfolios/machanical engineer/index.html" target="_blank">View Portfolio</a>
+        </div>`;
